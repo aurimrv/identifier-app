c5a3dfc02af3b68c05e284e658219e6ce5f21ae5
Auri Vincenzi <aurimrv@yahoo.com.br>
2023-08-10 14:46:14 -0300

    feat: enable bugs

--- app/src/test/java/app/identifier/IdentifierInitialTest.java
+++ app/src/test/java/app/identifier/IdentifierInitialTest.java
@@ -13,12 +13,5 @@ public class IdentifierInitialTest {
         Identifier id = new Identifier();
         boolean result = id.validateIdentifier("a1");
         assertEquals(true, result);
-    }
-
-    @Test
-    public void validateIdentifier02() {
-        Identifier id = new Identifier();
-        boolean result = id.validateIdentifier("");
-        assertEquals(false, result);
-    }    
+    } 
 }
